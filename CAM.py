b83634a9ef05c52c94a92a5fa8b77e25aebbd345
arshiyaimani <arshiyaimani3@gmail.com>
2023-04-16 13:41:00 +0330

    Update CAM.py

--- CAM.py
+++ CAM.py
@@ -2,7 +2,7 @@ import requests
 import time
 import os
 
-url = "YOUR ONLINE CAMERA LINK LIKE: http://128.65.187.44:85/webcapture.jpg?command=snap&channel=7?1681636203"
+links = ["http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=1?6", "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=2?6", "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=3?6" , "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=4?6" , "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=5?6" , "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=6?6" , "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=7?6" , "http://5.201.187.32:8080/webcapture.jpg?command=snap&channel=8?6" ]
 directory = "images"
 
 if not os.path.exists(directory):
@@ -10,7 +10,7 @@ if not os.path.exists(directory):
 
 i = 0
 while True:
-    response = requests.get(url)
+    response = requests.get(links[i % len(links)])
 
     filename = f"{directory}/image_{i}.jpg"
     while os.path.exists(filename):
@@ -21,4 +21,6 @@ while True:
         f.write(response.content)
     print(f"Image saved: {filename}")
     i += 1
+    if i == 8:
+        i = 0   # Reset the index to start over with the first link
     time.sleep(2)
